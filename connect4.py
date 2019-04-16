82420226d9ff110f8d3174179788878c20dc0cb2
saksiva <sakatheesan@gmail.com>
2019-04-16 12:24:12 +0100

    connect 4 in command line

--- connect4.py
+++ connect4.py
@@ -5,7 +5,7 @@ COLUMN_COUNT = 7
 
 
 def create_board():
-    board = np.zeros((6, 7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
 
@@ -14,7 +14,7 @@ def drop_piece(board, row, col, piece):
 
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT - 1][col] == 0
 
 
 def get_next_open_row(board, col):
@@ -25,6 +25,32 @@ def get_next_open_row(board, col):
 
 def print_board(board):
     print(np.flip(board, 0))
+
+
+def winning_move(board, piece):
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
+                c + 3] == piece:
+                return True
+
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
+                c] == piece:
+                return True
+
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][
+                c + 3] == piece:
+                return True
+
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][
+                c + 3] == piece:
+                return True
 
 
 board = create_board()
@@ -40,12 +66,20 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
 
+            if winning_move(board, 1):
+                print("WINNER PLAYER 1")
+                game_over = True
+
     else:
         col = int(input("Player 2 Make your selection (0-6): "))
 
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+
+            if winning_move(board, 2):
+                print("WINNER PLAYER 2")
+                game_over = True
 
     print_board(board)
     turn += 1
